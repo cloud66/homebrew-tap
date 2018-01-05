4067dc6aaf2352a5e4975204d9ee39ab3348aa30
Khash Sajadi <khash@cloud66.com>
2018-01-05 13:01:16 +0000

    gifnoc

---
+++ gifnoc.rb
@@ -0,0 +1,77 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+class Gifnoc < Formula
+  desc "Gifnoc, the cloud friendly configuration and security manager"
+  homepage "http://www.gifnoc.com"
+  version "0.0.1"
+  url "https://s3.amazonaws.com/downloads.cloud66.com/gifnoc/0.0.1/gifnoc_0.0.1.tar"
+  sha256 "be9521c4f09afca7f5365ac7f669fe95770ca8005b9cd17f680f449fd88451b6"
+
+  option "token=", "Your gifnoc's hosted name token (ie cloud66)"
+
+  def default_agent_token
+    "xxx"
+  end
+
+  def agent_token
+    ARGV.value("token") || default_agent_token
+  end
+
+  plist_options :manual => "gifnoc start --runas proxy --upstream https://cloud66.gifnoc.com --port 9820 --binding 127.0.0.1 --ignore-cert-errors --offline"
+
+  def plist
+    <<-EOS.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}/bin</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{HOMEBREW_PREFIX}/bin/gifnoc</string>
+          <string>start</string>
+          <string>--runas</string>
+          <string>proxy</string>
+          <string>--upstream</string>
+          <string>https://#{agent_token}.gifnoc.com</string>
+          <string>--port</string>
+          <string>9820</string>
+          <string>--binding</string>
+          <string>127.0.0.1</string>
+          <string>--ignore-cert-errors</string>
+          <string>--offline</string>
+        </array>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <key>PATH</key>
+          <string>#{HOMEBREW_PREFIX}/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+        </dict>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProcessType</key>
+        <string>Interactive</string>
+        <key>ThrottleInterval</key>
+        <integer>30</integer>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/gifnoc.log</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/gifnoc.log</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  def install
+    bin.install "gifnoc"
+  end
+
+  test do
+    system "gifnoc help"
+  end
+
+end
